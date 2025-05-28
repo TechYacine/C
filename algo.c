aefd8bdd4aaa2c6606c868d31e6044726c718b62
TechYacine <yacine.hamiham@gmail.com>
2025-05-28 19:38:13 +0100

    updated

--- algo.c
+++ algo.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
 int main(){
-    int v; 
-    printf("Choose a value");
-    scanf("%d", &v);
-    for(int i = 3 ; i != v; i++){
-        printf("hi \n");
-    }
+    
     return 0;
 }
--- strings/calc_string_sol.c
+++ strings/calc_string_sol.c
@@ -5,11 +5,11 @@ int main() {
     char ch[100]; // Buffer to store the input string
     printf("Enter a string terminated by '.': ");
     scanf("%s", ch); // Read the input string
-
+    
     int length = 0;
     int lowercaseCount = 0;
     int uppercaseCount = 0;
-
+    
     // Iterate through the string until '.' or '\0' is found
     while (ch[length] != '.' && ch[length] != '\0') {
         if (ch[length] >= 'a' && ch[length] <= 'z') {
@@ -19,7 +19,6 @@ int main() {
         }
         length++;
     }
-
     // Display the results
     printf("Number of characters: %d\n", length);
     printf("Number of lowercase alphabetic characters: %d\n", lowercaseCount);
